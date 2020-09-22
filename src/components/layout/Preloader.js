91013d9575220cd1b14c4f5adfb0f01eba2bd2d8
Lee Castelani <notayogilee@gmail.com>
2020-09-22 15:54:59 -0400

    add redux to get logs

--- src/components/layout/Preloader.js
+++ src/components/layout/Preloader.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Preloader = () => {
   return (
     <div className="progress blue lighten-4">
-      <div className="indeterminate-blue"></div>
+      <div className="indeterminate blue"></div>
     </div>
-  )
+  );
 };
 
 export default Preloader;
