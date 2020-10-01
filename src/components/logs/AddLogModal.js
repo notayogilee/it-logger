d52aea5213efbaa3549db08338ac82ca56911fa0
Lee Castelani <notayogilee@gmail.com>
2020-10-01 15:10:48 -0400

    add new tech to techs

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import TechSelectOptions from '../techs/TechSelectOptions';
 import PropTypes from 'prop-types';
 import { addLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -57,9 +58,7 @@ const AddLogModal = ({ addLog }) => {
               className="browser-default"
               onChange={e => setTech(e.target.value)}>
               <option value="" disabled>Select Technician</option>
-              <option value="John Doe" >John Doe</option>
-              <option value="Sam Smith" >Sam Smith</option>
-              <option value="Sara Wilson" >Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
